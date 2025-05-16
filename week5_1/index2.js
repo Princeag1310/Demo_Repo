f660115e39e2072d8cf77c39eb2c5fab0967b991
Prince Agrawal <princeagrawal1013@gmail.com>
2025-05-16 14:45:54 +0530

    Do this

---
+++ week5_1/index2.js
@@ -0,0 +1,13 @@
+//axios vs fetch
+
+function main(){
+    fetch("https://sum-server.100xdevs.com/todos")
+    .then(async response=>{
+        const json = await response.json();
+        console.log(json);
+        // await response.text();
+
+    });
+}
+
+main();
