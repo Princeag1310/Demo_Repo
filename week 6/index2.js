38540f3434e274b9e1b3030dbeca6379240378ba
Prince Agrawal <princeagrawal1013@gmail.com>
2025-05-22 10:14:31 +0530

    Okay1

---
+++ week 6/index2.js
@@ -0,0 +1,113 @@
+const express = require('express');
+const jwt = require('jsonwebtoken');
+
+const JWT_SECRET = "prince123"
+
+const app = express();
+
+app.use(express.json());
+
+const users = [];
+
+function logger(req, res, next){
+    console.log(req.method+ " request came ");
+    next();
+}
+
+app.get("/", function(req, res){
+    res.sendFile(__dirname + "/public/index.html");
+})
+
+app.post("/signup",logger, function(req, res){
+    const username = req.body.username;
+    const password = req.body.password;
+
+    users.push({
+        username: username,
+        password: password
+    })
+
+    res.json({
+        message: "You are signed up"
+    })
+
+    //we should check if a user with this username already exists
+
+})
+
+
+app.post("/signin",logger, function(req, res){
+    const username = req.body.username;
+    const password = req.body.password;
+
+    let foundUser = null;
+    
+    for(let i=0;i<users.length;i++){
+        if(users[i].username === username && users[i].password === password){
+            foundUser = users[i];
+        }
+    }
+
+    if(!foundUser){
+        res.json({
+            message: "Credentials are incorrect"
+        })
+        return;
+    }
+    else{
+        const token = jwt.sign({
+            username:foundUser.username
+        }, JWT_SECRET);
+        res.json({
+            token: token
+        })
+    }
+
+})
+
+function auth(req, res, next){
+    const token = req.headers.token;
+    const decodedData = jwt.verify(token, JWT_SECRET);
+    if(decodedData.username){
+        req.username = decodedData.username;
+        next();
+    }
+    else{
+        res.json({
+            message: "You are not logged in"
+        })
+    }
+}
+
+
+app.get("/me", logger, auth, function(req, res){
+    const token = req.headers.token;
+    const decodedData = jwt.verify(token, JWT_SECRET);
+    // const decodedData = jwt.decode(token);
+
+    if(decodedData.username){
+        let foundUser = null;
+
+        for(let i=0;i<users.length;i++){
+            if(users[i].username === req.username){
+                foundUser = users[i];
+            }
+        }
+        res.json({
+            username: foundUser.username,
+            password: foundUser.password
+        })
+    }
+})
+
+app.get("/todo",logger, auth, function(req, res){
+
+})
+app.post("/todo",logger, auth, function(req, res){
+
+})
+app.delete("/todo",logger, auth, function(req, res){
+
+})
+
+app.listen(3000);
